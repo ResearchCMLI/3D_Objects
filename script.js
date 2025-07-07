2da03f55abd5e4e8f5077ff43811c73e219630e3
ResearchCMLI <cent_ml_mi@avinuty.ac.in>
2025-07-07 12:05:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+import * as THREE from 'https://unpkg.com/three@0.155.0/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.155.0/examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const light = new THREE.HemisphereLight(0xffffff, 0x444444, 1.2);
+scene.add(light);
+
+const loader = new GLTFLoader();
+loader.load('model.glb', function (gltf) {
+  scene.add(gltf.scene);
+  gltf.scene.position.set(0, 0, 0);
+}, undefined, function (error) {
+  console.error(error);
+});
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
